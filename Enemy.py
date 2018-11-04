9038aa6509ea6650e87b14c680b241c8d34abcdb
Ryaib <phindenburgh@gmail.com>
2018-11-03 22:07:11 -0700

    bug fix

--- Enemy.py
+++ Enemy.py
@@ -92,7 +92,7 @@ class Enemy(Sprite):
 
     def shell_hit(self):
         if not self.shell_sound:
-            pygame.mixer.music.load('sound\kick.ogg')
+            pygame.mixer.music.load('assets/sound/kick.ogg')
             pygame.mixer.music.play(0)
             self.shell_sound = True
         current = pygame.time.get_ticks()
