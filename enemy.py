d85b39d8c03439d49dffdbcf5fe485fe7ba1e5b9
Ryaib <phindenburgh@gmail.com>
2018-11-07 22:39:42 -0800

    added active flag

--- enemy.py
+++ enemy.py
@@ -15,6 +15,7 @@ class Enemy:
         self.__visible_tiles = []
 
         self.frame = 0
+        self.active = False
 
     def set_visible_tiles(self, visible_tiles):
         self.__visible_tiles = visible_tiles
@@ -24,14 +25,16 @@ class Enemy:
         self.total_height = total_height
 
     def update(self):
-        self.frame += 1
+        if self.active:
+            self.frame += 1
+            self.tile.rect.x += -1
 
-        if self.frame % 30 == 0:
-            if self.enemy_type == "goomba":
-                if self.tile.image is self.tile.images["goomba_two"]:
-                    self.tile.image = self.tile.images["goomba_one"]
-                else:
-                    self.tile.image = self.tile.images["goomba_two"]
+            if self.frame % 30 == 0:
+                if self.enemy_type == "goomba":
+                    if self.tile.image is self.tile.images["goomba_two"]:
+                        self.tile.image = self.tile.images["goomba_one"]
+                    else:
+                        self.tile.image = self.tile.images["goomba_two"]
 
     def render(self, x, y):
         self.tile.render(x, y)
