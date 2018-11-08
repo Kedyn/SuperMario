eeb54342836d9ae999777a5e384e4b4f5fe9d835
Ryaib <phindenburgh@gmail.com>
2018-11-07 21:31:35 -0800

    slowed ? animation

--- interactive_tile.py
+++ interactive_tile.py
@@ -2,11 +2,14 @@ class InteractiveTile:
     def __init__(self, tile):
         self.tile = tile
 
+        self.frame = 0
     def update(self):
-        if self.tile.tile_type is '?':
-            if self.tile.image is self.tile.images['mystery_box_bright']:
-                self.tile.image = self.tile.images['mystery_box_normal']
-            elif self.tile.image is self.tile.images['mystery_box_normal']:
-                self.tile.image = self.tile.images['mystery_box_dark']
-            else:
-                self.tile.image = self.tile.images['mystery_box_bright']
+        self.frame += 1
+        if self.frame % 10 == 0:
+            if self.tile.tile_type is '?':
+                if self.tile.image is self.tile.images['mystery_box_bright']:
+                    self.tile.image = self.tile.images['mystery_box_normal']
+                elif self.tile.image is self.tile.images['mystery_box_normal']:
+                    self.tile.image = self.tile.images['mystery_box_dark']
+                else:
+                    self.tile.image = self.tile.images['mystery_box_bright']
